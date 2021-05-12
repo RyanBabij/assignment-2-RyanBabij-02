55418372f6de90b9c08989f572912c731198305d
Ryan Babij <rbabij@protonmail.com>
2021-05-12 15:48:10 +1000

    Replace old login fxml with new

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ui/login.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("ui/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/Login-02.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Login");
