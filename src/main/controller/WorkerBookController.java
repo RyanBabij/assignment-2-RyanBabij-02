27636240008af974c3eecf1455465bf4e81e8b50
Ryan Babij <rbabij@protonmail.com>
2021-05-18 16:54:12 +1000

    Add choicebox and date default values

--- src/main/controller/WorkerBookController.java
+++ src/main/controller/WorkerBookController.java
@@ -3,15 +3,37 @@ package main.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import main.Main;
 import main.MenuWorkerMain;
+import java.time.LocalDateTime;
+
+// We need:
+// Timeslots
+// Seat
+// Max for given seat and timeslot.
+
+// Every time you pick a choice the remaining choices should narrow down based on the possibilities
+
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
 
 public class WorkerBookController
 {
 
     @FXML
     private ListView fxBookingList;
+    @FXML
+    private ChoiceBox fxChoiceTime;
+    @FXML
+    private ChoiceBox fxChoiceDuration;
+    @FXML
+    private ChoiceBox fxChoiceSeat;
+    @FXML
+    private DatePicker fxDate;
 
     public WorkerBookController()
     {
@@ -20,6 +42,20 @@ public class WorkerBookController
     @FXML
     public void initialize()
     {
+        fxChoiceTime.getItems().add("None");
+        fxChoiceDuration.getItems().add("None");
+        fxChoiceDuration.getItems().add("Choice 2");
+        fxChoiceDuration.getItems().add("Choice 3");
+        fxChoiceSeat.getItems().add("None");
+        fxChoiceSeat.getItems().add("1A");
+        fxChoiceSeat.getItems().add("1B");
+        fxChoiceSeat.getItems().add("1C");
+
+        fxChoiceTime.setValue("None");
+        fxChoiceDuration.setValue("None");
+        fxChoiceSeat.setValue("None");
+
+        fxDate.setValue(LocalDate.now()  );
     }
 
     // Logout worker and return to login screen
@@ -33,6 +69,7 @@ public class WorkerBookController
         ((Node)(event.getSource())).getScene().getWindow().hide();
     }
 
+    // Push the booking into the db
     public void MakeBooking(ActionEvent event)
     {
         System.out.println("MAKE BOOKING");
